236e6ad6c50219a66062138fd9177bef7f491f25
Kyle Pilon <91489409+kyle-pilon@users.noreply.github.com>
2025-01-05 18:54:09 -0500

    Time: 8 ms (34.63%) | Memory: 24.2 MB (6.67%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {} # HashMap to count occurances
+        freq = [[] for i in range(len(nums) + 1)] # Index : Count, List : #'s occuring at count
+        res = [] # Result list
+
+        # Count occurances with HashMap
+        for number in nums:
+            count[number] = 1 + count.get(number, 0)
+        # Use HashMap to build the frequency array
+        for number, count in count.items(): # Iterate over HashMap Key : Value pairs
+            freq[count].append(number)
+
+        # Iterate over the frequency array from len(freq) - 1 -> 0, (-1, 3rd aru)
+        for i in range(len(freq) - 1, 0, -1):
+            for number in freq[i]:
+                res.append(number)
+                if len(res) == k:
+                    return res
+        
