79731636cb66d4a01091ba25795e7ea1263d98ea
Kyle Pilon <91489409+kyle-pilon@users.noreply.github.com>
2025-01-06 19:42:53 -0500

    Time: 3 ms (83.45%) | Memory: 17.7 MB (25.03%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,20 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = collections.defaultdict(set)
-        cols = collections.defaultdict(set)
-        groups = collections.defaultdict(set)
+        rows = collections.defaultdict(set) # Key : r
+        cols = collections.defaultdict(set) # Key : c
+        groups = collections.defaultdict(set) # Key : (r // 3, c // 3)
 
         for r in range(len(board)):
             for c in range(len(board[r])):
-                if board[r][c] == ".":
+                if board[r][c] == ".": # Continue to pass non-numbers
                     continue
-                if board[r][c] in rows[r]:
+                if board[r][c] in rows[r]: # If exists in row HashSet
                     return False
-                if board[r][c] in cols[c]:
+                if board[r][c] in cols[c]: # If exists in column HashSet
                     return False
-                if board[r][c] in groups[(r // 3, c // 3)]:
+                if board[r][c] in groups[(r // 3, c // 3)]: # If exists in 3x3 group HashSet
                     return False
-                rows[r].add(board[r][c])
+                rows[r].add(board[r][c]) 
                 cols[c].add(board[r][c])
                 groups[(r // 3, c // 3)].add(board[r][c])
         return True
