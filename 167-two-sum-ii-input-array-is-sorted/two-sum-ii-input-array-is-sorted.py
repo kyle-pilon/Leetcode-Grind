d3c21ab73a1df8e56a0551339485f79ccf5ec42e
Kyle Pilon <91489409+kyle-pilon@users.noreply.github.com>
2025-01-07 19:41:22 -0500

    Time: 7 ms (27.81%) | Memory: 18.5 MB (32.97%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -4,7 +4,7 @@ class Solution:
         while left < right:
             total = numbers[left] + numbers[right]
             if total == target:
-                return [left + 1, right + 1]
+                return [left + 1, right + 1] # Add 1 to both indices because we want 1-indexed array
             if total > target:
                 right -= 1
             if total < target:
