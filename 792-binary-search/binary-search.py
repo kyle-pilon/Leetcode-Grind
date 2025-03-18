e2c8d1abf0e15df681a2eae7762e61c70e0d1833
Kyle Pilon <91489409+kyle-pilon@users.noreply.github.com>
2025-03-18 19:47:13 -0400

    Time: 0 ms (100.00%) | Memory: 18.8 MB (27.96%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,13 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
-
-        while left <= right:
-            mid = left + ((right - left) // 2) # (left + right) // 2 can lead to overflow
-            if target > nums[mid]:
+        while (left <= right):
+            mid = left + (right - left) // 2
+            if (nums[mid] < target):
                 left = mid + 1
-            elif target < nums[mid]:
+            elif (nums[mid] > target):
                 right = mid - 1
-            elif target == nums[mid]:
-                return mid 
+            elif (nums[mid] == target):
+                return mid
         return -1
